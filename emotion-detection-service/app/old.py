13a1bc3c155a96c40121fd61cd95dd9efd48ceca
Hafizh Abid Wibowo <hafizhabidw@gmail.com>
2023-06-16 02:10:12 +0700

    fix: move from onnxruntime to torch

--- emotion-detection-service/app/old.py
+++ emotion-detection-service/app/old.py
@@ -1,22 +1,21 @@
 import numpy as np
 import cv2
 import time
+import onnx
+import onnxruntime as rt
 import os
 import mediapipe as mp
 import paho.mqtt.client as mqtt
 import json
-import torch
-from torchvision import transforms
-import timm
-from PIL import Image
 
-# client = mqtt.Client(client_id='emotion-detection-service')
-# mqtt_hostname = os.environ.get('MQTT_HOSTNAME')
-# client.connect(host = mqtt_hostname, port = 1883)
-# client.loop_start()
+client = mqtt.Client(client_id='emotion-detection-service')
 
-device = "cpu"
-img_size = 260
+mqtt_hostname = os.environ.get('MQTT_HOSTNAME')
+
+client.connect(host = mqtt_hostname, port = 1883)
+
+client.loop_start()
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 cv2.ocl.setUseOpenCL(False)
 
@@ -33,58 +32,45 @@ def detect_face(frame):
             y2 = min(height,(bb.ymin+bb.height)*height)
             bbox = np.array([x1,y1,x2,y2])
             bounding_boxes.append(bbox)
+
     return bounding_boxes
 
-def get_probab(features):
-    x = np.dot(features,np.transpose(classifier_weights)) + classifier_bias
-    return x
+def preprocess(img):
+    x = cv2.resize(img,(260,260))/255
+    x[..., 0] = (x[..., 0]-0.485)/0.229
+    x[..., 1] = (x[..., 1]-0.456)/0.224
+    x[..., 2] = (x[..., 2]-0.406)/0.225
+    return x.transpose(2, 0, 1).astype("float32")[np.newaxis,...]
 
-def extract_features(face_img):
-    img_tensor = test_transforms(Image.fromarray(face_img))
-    img_tensor.unsqueeze_(0)
-    features = model(img_tensor.to(device))
-    features = features.data.cpu().numpy()
-    return features
-    
 def predict_emotions(face_img):
-    features = extract_features(face_img)
-    scores = get_probab(features)[0]
+    scores = m.run(None,{"input": preprocess(face_img)})[0][0]
     x = scores
     pred = np.argmax(x)
-    return idx_to_class[pred],scores
+    return label[pred], pred
 
-idx_to_class = {0: 'Anger', 1: 'Contempt', 2: 'Disgust', 3: 'Fear', 4: 'Happiness', 5: 'Neutral', 6: 'Sadness', 7: 'Surprise'}
+model = "./model/enet_b2_8.onnx"
+onnxModel = onnx.load(model)
 
-test_transforms = transforms.Compose(
-    [
-        transforms.Resize((img_size, img_size)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean = [0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ]
-)
+output_names = [n.name for n in onnxModel.graph.output]
 
-path = "./model/enet_b2_8_best.pt"
-model = torch.load(path, map_location = torch.device(device))
-classifier_weights = model.classifier.weight.cpu().data.numpy()
-classifier_bias = model.classifier.bias.cpu().data.numpy()
-model.classifier = torch.nn.Identity()
-model = model.to(device)
-model = model.eval()
-print(path, test_transforms)
+# dictionary which assigns each label an emotion (alphabetical order)
+label = {0: 'Anger', 1: 'Contempt', 2: 'Disgust', 3: 'Fear', 4: 'Happiness', 5: 'Neutral', 6: 'Sadness', 7: 'Surprise'}
 
+m = rt.InferenceSession(model, providers=['CPUExecutionProvider'])
 
 previous_time = int(time.time())
 threshold = 1
 
 cap = cv2.VideoCapture(0)
 while True:
+
+    # Find haar cascade to draw bounding box around face
     ret, frame = cap.read()
     
     if not ret:
         break
 
     current_time = int(time.time())
-
     if current_time - previous_time >= threshold:
 
         mp_face_detection = mp.solutions.face_detection
@@ -106,14 +92,14 @@ while True:
                 'time': current_time
             }
             
-            #info = client.publish(topic = "emotion", payload = json.dumps(res))
-            #info.wait_for_publish()
+            info = client.publish(topic = "emotion", payload = json.dumps(res))
+            info.wait_for_publish()
             #print(emotion, info.is_published())
-            # cv2.putText(frame, emotion, (x1, y1-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame, emotion, (x1, y1-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         
         previous_time = current_time
 
-    # cv2.imshow('Video', cv2.resize(frame,(1280,720),interpolation = cv2.INTER_CUBIC))
+    cv2.imshow('Video', cv2.resize(frame,(1280,720),interpolation = cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
